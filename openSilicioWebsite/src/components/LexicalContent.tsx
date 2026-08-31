d228ab18303fa52a70e280f49ee7984dc6398dfd
Paulo Campos <paulocampos-dev@outlook.com>
2026-08-31 10:49:25 -0300

    Revert "feat(educacao): add a copy button and language label to code blocks"
    This reverts commit 86cbbb546689b0c6496496a27dea7ca5d8704ff0.

--- openSilicioWebsite/src/components/LexicalContent.tsx
+++ openSilicioWebsite/src/components/LexicalContent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Box, useTheme, Skeleton, Stack } from '@mui/material';
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
@@ -6,7 +6,6 @@ import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import LexicalErrorBoundary from './lexical/LexicalErrorBoundary';
 import { LEXICAL_NODES } from './lexical/nodeSet';
 import { LEXICAL_THEME } from './lexical/theme';
-import { useBotoesDeCopiar } from './lexical/CopiarCodigo';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 
 interface LexicalContentProps {
@@ -43,8 +42,6 @@ function LexicalContentInner() {
 
 export default function LexicalContent({ content }: LexicalContentProps) {
   const theme = useTheme();
-  const containerRef = useRef<HTMLDivElement>(null);
-  useBotoesDeCopiar(containerRef, content);
 
   // Don't render if no content or invalid format
   if (!content) {
@@ -87,7 +84,6 @@ export default function LexicalContent({ content }: LexicalContentProps) {
 
   return (
     <Box
-      ref={containerRef}
       sx={{
         '& .ContentEditable__root': {
           outline: 'none',
